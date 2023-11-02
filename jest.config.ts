60e5adb8bfcdaffc9acd5130249930fe5d92a9cf
ikrasnikov <krasenzhenya@gmail.com>
2023-11-02 15:52:48 +0200

    init commit

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'org',
+  displayName: 'panel-label',
   preset: './jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: './coverage/org',
+  coverageDirectory: './coverage/panel-label',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'org-root',
+  selector: 'panel-label-constructor-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
